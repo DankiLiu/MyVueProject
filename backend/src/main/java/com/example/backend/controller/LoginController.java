12c01c2afc554dfc0dfb5c139379c7ec858bf08a
Danki <liudanqing1996@hotmail.com>
2021-04-23 23:11:19 +0200

    Initialize my first Vue & Spring-Boot project

---
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.example.backend.controller;
+
+
+import com.example.backend.pojo.User;
+import com.example.backend.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Objects;
+
+@Controller
+public class LoginController {
+
+    @CrossOrigin
+    @PostMapping
+    @ResponseBody
+
+    public Result login(User requestUser){
+        String username = requestUser.getUsername();
+        username = HtmlUtils.htmlEscape(username);
+
+        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
+    }
+}
